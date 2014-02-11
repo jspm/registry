3bc5a1f4f6287ca15f3d984c5e4aa239f6e2c6fb
guybedford <guybedford@gmail.com>
2014-02-11 17:30:08 +0200

    cli updates

--- test/config.js
+++ test/config.js
@@ -1,22 +1,19 @@
 
-System.paths['npm:*'] = 'jspm_packages/npm/*.js';
-System.paths['github:*'] = 'jspm_packages/github/*.js';
-System.paths['registry-tests/*'] = 'tests/*.js';
 System.paths['tests/*'] = 'tests/*.js';
 
 System.map = {
-  'mocha': 'npm:mocha@^1.17.1',
-  'angular': 'github:angular/bower-angular@^1.2.12',
   'text': 'github:systemjs/plugin-text@master',
+  'angular': 'github:angular/bower-angular@^1.2.12',
+  'mocha': 'npm:mocha@^1.17.1',
   'npm:mocha@1.17.1': {
     'css': 'github:systemjs/plugin-css@master'
   }
 };
 
 System.versions = {
+  'github:systemjs/plugin-text': 'master',
   'github:angular/bower-angular': '1.2.12',
   'npm:mocha': '1.17.1',
-  'github:systemjs/plugin-css': 'master',
-  'github:systemjs/plugin-text': 'master'
+  'github:systemjs/plugin-css': 'master'
 };
 
